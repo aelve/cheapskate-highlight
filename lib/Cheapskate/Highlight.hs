715d67fbc213fdb4b63643977495c1eefa7e4aa0
Artyom <yom@artyom.me>
2016-03-15 02:05:23 +0300

    More code

--- lib/Cheapskate/Highlight.hs
+++ lib/Cheapskate/Highlight.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE
+RecordWildCards
+  #-}
+
+
 module Cheapskate.Highlight
 (
   -- * Highlighting
@@ -13,12 +18,12 @@ module Cheapskate.Highlight
   -- * Styles
   module Text.Highlighting.Kate.Styles,
   styleToCss,
-  styleToLaTeX,
 )
 where
 
 
 import qualified Data.Text as T
+import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 import Cheapskate
 import Text.Highlighting.Kate
@@ -26,26 +31,34 @@ import Text.Highlighting.Kate.Styles
 import Text.Blaze.Html.Renderer.Text
 
 
-highlightDoc :: Doc -> Doc
-highlightDoc doc = walk highlightBlock doc
+{- |
+Highlight a document by replacing code blocks with raw HTML blocks.
 
-highlightDocWith :: FormatOptions -> Doc -> Doc
-highlightDocWith opts doc = walk (highlightBlockWith opts) doc
+By default, the rest of the attribute line (i.e. all words after the first word after @~~~@ or @```@) get added as classes to the container block of the code.
+-}
+highlightDoc :: Doc -> Doc
+highlightDoc = highlightDocWith (\_ _ x -> x)
+
+{- |
+The function is given code block's language (i.e. the 1st word of the attribute line after @~~~@ or @```@) and the rest of the attribute line.
+
+If you don't want the classes to be derived from the attribute line, make the function set 'containerClasses' to @[]@.
+-}
+highlightDocWith
+  :: (Text -> Text -> FormatOptions -> FormatOptions) -> Doc -> Doc
+highlightDocWith f doc = walk (highlightBlockWith f) doc
 
 highlightBlock :: Block -> Block
-highlightBlock (CodeBlock attr code) =
-  HtmlBlock (TL.toStrict (renderHtml formatted))
-  where
-    lang = T.unpack (codeLang attr)
-    highlighted = highlightAs lang (T.unpack code)
-    formatted = formatHtmlBlock defaultFormatOpts highlighted
-highlightBlock other = other
+highlightBlock = highlightBlockWith (\_ _ x -> x)
 
-highlightBlockWith :: FormatOptions -> Block -> Block
-highlightBlockWith opts (CodeBlock attr code) =
+highlightBlockWith
+  :: (Text -> Text -> FormatOptions -> FormatOptions) -> Block -> Block
+highlightBlockWith f (CodeBlock CodeAttr{..} code) =
   HtmlBlock (TL.toStrict (renderHtml formatted))
   where
-    lang = T.unpack (codeLang attr)
+    lang = T.unpack codeLang
     highlighted = highlightAs lang (T.unpack code)
-    formatted = formatHtmlBlock opts highlighted
+    opts = defaultFormatOpts {
+             containerClasses = words (T.unpack codeInfo) }
+    formatted = formatHtmlBlock (f codeLang codeInfo opts) highlighted
 highlightBlockWith _ other = other
